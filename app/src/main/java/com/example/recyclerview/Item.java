91610674f25a1668da2cfcb23765757d05455cf2
Irfan Fauzi <broirfanfauzi@gmail.com>
2024-06-05 11:28:42 +0700

    Initial commit

---
+++ app/src/main/java/com/example/recyclerview/Item.java
@@ -0,0 +1,17 @@
+package com.example.recyclerview;
+
+public class Item {
+    private String text;
+
+    public Item(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
